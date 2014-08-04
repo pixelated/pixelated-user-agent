d60b328acb512def6f1925b2969d60c4cafd337b
Bruno Wagner Goncalves <bwagner@thoughtworks.com>
2014-08-04 20:48:18 -0300

    Changed identation to 4 spaces to conform to rest of the code

--- service/test/bitmask_libraries/abstract_leap_test.py
+++ service/test/bitmask_libraries/abstract_leap_test.py
@@ -6,21 +6,24 @@ from mock import Mock, MagicMock
 
 
 class AbstractLeapTest(unittest.TestCase):
-        uuid = str(uuid4())
-        session_id = str(uuid4())
-        token = str(uuid4())
+    uuid = str(uuid4())
+    session_id = str(uuid4())
+    token = str(uuid4())
 
-        leap_home = os.path.join(tempfile.mkdtemp(), 'leap')
+    leap_home = os.path.join(tempfile.mkdtemp(), 'leap')
 
-        config = Mock(leap_home=leap_home, ca_cert_bundle='/some/path/to/ca_cert', gpg_binary='/path/to/gpg')
-        provider = Mock(config=config, server_name='some-server.test', domain='some-server.test',
-                        api_uri='https://api.some-server.test:4430', api_version='1')
-        soledad = Mock()
-        soledad_session = Mock(soledad=soledad)
-        srp_session = Mock(user_name='test_user', api_server_name='some-server.test', uuid=uuid, session_id=session_id, token=token)
+    config = Mock(leap_home=leap_home, ca_cert_bundle='/some/path/to/ca_cert', gpg_binary='/path/to/gpg')
+    provider = Mock(config=config, server_name='some-server.test', domain='some-server.test',
+                    api_uri='https://api.some-server.test:4430', api_version='1')
+    soledad = Mock()
+    soledad_session = Mock(soledad=soledad)
+    srp_session = Mock(user_name='test_user', api_server_name='some-server.test', uuid=uuid, session_id=session_id, token=token)
 
-        nicknym = MagicMock()
+    nicknym = MagicMock()
 
-        soledad_account = MagicMock()
+    soledad_account = MagicMock()
 
-        mail_fetcher_mock = MagicMock()
+    mail_fetcher_mock = MagicMock()
+
+    def tearDown():
+        reload
