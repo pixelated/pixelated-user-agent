71d49858f9c5e6e4e7ea86d137f0c8300456382e
Thiago Colucci and Patrick Maia <pixelated-team+colucci+patrick@thoughtworks.com>
2014-08-08 17:02:23 -0300

    adds ability to define behavior when a feature is toggled off

--- web-ui/app/js/mixins/with_feature_toggle.js
+++ web-ui/app/js/mixins/with_feature_toggle.js
@@ -1,14 +1,17 @@
 define(['features'],
   function(features) {
 
-    function withFeatureToggle(componentName) {
+    function withFeatureToggle(componentName, behaviorForFeatureOff) {
       return function() {
 
-        this.around('initialize', function(basicInitialize) {
+        this.around('initialize', _.bind(function(basicInitialize) {
           if(features.isEnabled(componentName)) {
             return basicInitialize(arguments[1], arguments[2]);
           }
-        });
+          else if (behaviorForFeatureOff){
+            behaviorForFeatureOff.bind(this).call();
+          }
+        }, this));
       };
     }
 
