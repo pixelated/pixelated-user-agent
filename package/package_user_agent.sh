1cd9879175c6316cd723a30ba1670c71467a6242
Bruno Wagner Goncalves <bwagner@thoughtworks.com>
2014-08-18 20:45:03 -0300

    Update setup tools and pip before packaging

--- package/package_user_agent.sh
+++ package/package_user_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x 
+set -x
 
 PACKAGE_VERSION="$1"
 BUILD_PATH=/tmp/pix-user-agent-build
@@ -20,7 +20,7 @@ mkdir -p $PIXELATED_WEB_LIB_PATH
 mkdir -p $BUILD_PATH
 
 # build web-ui code
-cd web-ui 
+cd web-ui
 bundle install --path=~/pixelated-gems
 npm install
 node_modules/bower/bin/bower install
@@ -34,6 +34,8 @@ cp -rf web-ui/dist/* $PIXELATED_WEB_LIB_PATH
 # build virtual env
 virtualenv $PIXELATED_VIRTUALENV_PATH
 . $PIXELATED_VIRTUALENV_PATH/bin/activate
+pip install --upgrade pip
+pip install --upgrade setuptools
 easy_install leap.soledad.common
 pip install -r service/requirements.txt
 deactivate
