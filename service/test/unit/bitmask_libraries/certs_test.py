5c9aa9aff7fed5feddc58845c72607caa7feb76c
Folker Bernitt <fbernitt@thoughtworks.com>
2014-11-10 14:11:21 +0100

    Added test for bundle detection return type.

---
+++ service/test/unit/bitmask_libraries/certs_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from pixelated.bitmask_libraries.certs import which_bundle
+from pixelated.bitmask_libraries.config import AUTO_DETECT_CA_BUNDLE
+from mock import MagicMock, patch
+
+
+class CertsTest(unittest.TestCase):
+
+    @patch('pixelated.bitmask_libraries.certs.os.path.isfile')
+    def test_that_which_bundle_returns_byte_string(self, mock_isfile):
+        mock_isfile.return_value = True
+        config = MagicMock(ca_cert_bundle=AUTO_DETECT_CA_BUNDLE, certs_home='/some/path')
+        provider = MagicMock(server_name='test.leap.net', config=config)
+
+        bundle = which_bundle(provider)
+
+        self.assertEqual('/some/path/test.leap.net.ca.crt', bundle)
+        self.assertEqual(str, type(bundle))
