4f961b29b00f5ccde29d463b2ccb4a89d0783c38
Victor Shyba <victor.shyba@gmail.com>
2014-11-18 17:33:44 -0300

    sys was used, but not imported

--- service/test/unit/bitmask_libraries/smtp_test.py
+++ service/test/unit/bitmask_libraries/smtp_test.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
 import os
+import sys
 from mock import MagicMock, patch
 from abstract_leap_test import AbstractLeapTest
 from pixelated.bitmask_libraries.smtp import LeapSmtp
