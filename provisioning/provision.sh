5d6b14b54fb69c3a611cf1f03d9e8bf2d76a2cad
Mircea Moise <mmoise@thoughtworks.com>
2014-11-19 20:31:04 +0000

    Added vagrantfile and provisioning scripts so the service can be run inside a vagrant box

---
+++ provisioning/provision.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y npm
+sudo apt-get install -y python-setuptools
+sudo easy_install pip
+sudo pip install virtualenv
+sudo apt-get install -y python-dev
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y g++
+sudo gem install bundler
+
+/vagrant/provisioning/bootstrap.sh
