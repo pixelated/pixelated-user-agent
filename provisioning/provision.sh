b02c547e966c20484f2251fd9d244f94ddf031c7
ackackack <zenackzen@gmail.com>
2014-11-24 00:21:35 +0000

    Wording and script alterations
    Altered contents of:
    provision.sh 	- moved all apt-get's to a single line
    README.md	- Edited lines to read a little better than before

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,13 +1,7 @@
 sudo apt-get update
-sudo apt-get install -y git
-sudo apt-get install -y nodejs-legacy
-sudo apt-get install -y npm
-sudo apt-get install -y python-setuptools
+sudo apt-get install -y git nodejs-legacy npm python-setuptools python-dev libffi-dev g++
 sudo easy_install pip
 sudo pip install virtualenv
-sudo apt-get install -y python-dev
-sudo apt-get install -y libffi-dev
-sudo apt-get install -y g++
 sudo gem install bundler
 
 /vagrant/provisioning/bootstrap.sh
