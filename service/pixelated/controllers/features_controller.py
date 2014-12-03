f7472aed29c525bc630fe1ea05833f840bc42dc4
Folker Bernitt <fbernitt@thoughtworks.com>
2014-12-03 17:37:14 +0100

    Disabled encryption status again.
    - See issues #59 and #165

--- service/pixelated/controllers/features_controller.py
+++ service/pixelated/controllers/features_controller.py
@@ -19,7 +19,7 @@ import os
 
 
 class FeaturesController:
-    DISABLED_FEATURES = ['draftReply']
+    DISABLED_FEATURES = ['draftReply', 'encryptionStatus']
 
     def __init__(self):
         pass
