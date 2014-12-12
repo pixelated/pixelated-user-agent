dd02018037e9f0e5c04110c933cafac9b5557cc3
Patrick Maia <patrickjourdanmaia@gmail.com>
2014-12-12 11:59:53 +0000

    corrects test name

--- service/test/integration/contacts_test.py
+++ service/test/integration/contacts_test.py
@@ -2,13 +2,13 @@ from nose.twistedtools import deferred
 from test.support.integration import SoledadTestBase, MailBuilder
 
 
-class SearchTest(SoledadTestBase):
+class ContactsTest(SoledadTestBase):
 
     def setUp(self):
         SoledadTestBase.setUp(self)
 
     @deferred(timeout=SoledadTestBase.DEFERRED_TIMEOUT)
-    def test_that_tags_returns_all_tags(self):
+    def test_TO_CC_and_BCC_fields_are_being_searched(self):
         input_mail = MailBuilder().with_tags(['important']).build_input_mail()
         self.client.add_mail_to_inbox(input_mail)
 
