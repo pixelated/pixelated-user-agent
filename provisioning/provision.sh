c5e45b28aa99d926ed6f53b8155da56dfb366941
Bruno Wagner <bwgpro@gmail.com>
2014-12-12 17:16:24 -0200

    Updated provision after removal of gemfile

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,5 +1,7 @@
+export LC_ALL=en_US.UTF-8
+
 sudo apt-get update
-sudo apt-get install -y git nodejs-legacy npm python-setuptools python-dev libffi-dev g++ rng-tools
+sudo apt-get install -y git nodejs-legacy npm python-setuptools python-dev libffi-dev g++ rng-tools ruby-dev
 sudo easy_install pip
 sudo pip install virtualenv
 sudo gem install compass
