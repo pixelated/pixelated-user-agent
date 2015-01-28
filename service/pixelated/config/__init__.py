pixelated/pixelated-user-agent

commit 26b85d8e523f2b6aafbe18132efcd617ae892228

#237 #232 #196 syncing user data once before starting the UA. Displaying a loading screen in the meantime. The initial sync will make sure the user has a single key pair

service/pixelated/config/__init__.py
@@ -1,0 +1,71 @@
+#
+# Copyright (c) 2014 ThoughtWorks, Inc.
+#
+# Pixelated is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pixelated is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
+
+from functools import partial
+import sys
+
+from pixelated.config.app import App
+from pixelated.config import app_factory
+from pixelated.config.args import parse as parse_args
+from pixelated.config.config_ua import config_user_agent
+from pixelated.config.dispatcher import config_dispatcher
+from pixelated.config.events_server import init_events_server
+from pixelated.config.loading_page import loading
+from pixelated.config.register import register
+from pixelated.config.debug import init_debugger
+from pixelated.config.leap_cert import init_leap_cert
+from pixelated.config.soledad import init_soledad_and_user_key
+from twisted.internet import reactor
+from twisted.internet.threads import deferToThread
+
+
+def initialize():
+    args = parse_args()
+    app = App()
+
+    init_debugger(args)
+    init_leap_cert(args)
+
+    if args.register:
+        register(*args.register[::-1])
+        sys.exit(0)
+
+    if args.dispatcher or args.dispatcher_stdin:
+        config_dispatcher(app, args)
+    else:
+        config_user_agent(app, args)
+
+    init_events_server()
+
+    def load_app():
+        # welcome to deferred hell. Or maybe you'll be welcomed later, who knows.
+        loading_app = loading(args)
+
+        def init_soledad():
+            return init_soledad_and_user_key(app)
+
+        def stop_loading_app(leap_session):
+            d = loading_app.stopListening()
+            d.addCallback(partial(start_user_agent_app, leap_session))
+
+        def start_user_agent_app(leap_session, _):
+            app_factory.create_app(app, args, leap_session)
+
+        d = deferToThread(init_soledad)
+        d.addCallback(stop_loading_app)
+
+    reactor.callWhenRunning(load_app)
+    reactor.run()

service/pixelated/config/soledad.py
@@ -14,8 +14,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
 
-from config import initialize
+from pixelated.bitmask_libraries.session import open as open_leap_session
 
 
-if __name__ == '__main__':
-    initialize()
+def init_soledad_and_user_key(app):
+    leap_session = open_leap_session(app['LEAP_USERNAME'],
+                                     app['LEAP_PASSWORD'],
+                                     app['LEAP_SERVER_NAME'])
+    soledad = leap_session.soledad_session.soledad
+    soledad.sync(defer_decryption=False)
+    leap_session.nicknym.generate_openpgp_key()
+    return leap_session

service/__init__.py
@@ -1,0 +1,1 @@
+__author__ = 'pixelated'

service/pixelated/assets/Interstitial.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+<html style="border: 0px; padding: 0px; margin: 0px;">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script src="assets/snap.svg-min.js"></script>
+    <script src="assets/jquery.js"></script>
+  </head>
+
+  <body style="border: 0px; padding: 0px; margin: 0px;background-color: #808181">
+    <section id="hive-section" style="background-color: #808181;" name="hive-section">
+      <svg id="hive" style="width: 100%; height: 100%;"></svg>
+    </section>
+    <script src="assets/Interstitial.js"></script>
+  </body>
+
+</html>

service/pixelated/assets/Interstitial.js
@@ -1,0 +1,53 @@
+if ($('#hive').length) {
+  var hive = new Snap('#hive');
+  var img_width = $('#hive').width();
+  var left_pos = img_width * .5;
+
+  var pixelated = hive.path("M12.4,20.3v31.8l28,15.8l28-15.8V20.3l-28-15.8L12.4,20.3z M39.2,56.4l-16.3-9V27.9l16.3,9.3L39.2,56.4z M57.7,47.4l-16.1,9l0-19.2l16.1-9.4V47.4z M57.7,25.2L40.4,35.5L22.9,25.2l17.5-9.4L57.7,25.2z").transform("translate(319, 50)").attr("fill", "#908e8e");
+  var all = hive.group().transform("matrix(2, 0, 0, 2, "+(left_pos - 950)+", -40)");
+
+  var height = 50;
+  var width = 58;
+  var rows = $(window).height() / height;
+  var cols = (($(window).width() / width) / 2) + 1;
+
+  for(var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      x = i * width + (j%2*width/2);
+      y = j * height;
+      all.add(pixelated.clone().transform("translate("+x+","+y+")")); 
+    }
+  }
+
+  all.add(pixelated);
+  
+  function brightenLogo() {
+	  var glowPosition = Math.floor(Math.random()*rows*cols);
+
+	  all[glowPosition].animate({fill: "#FFF"}, 1000, function() {
+			  darkenLogo(all[glowPosition]);
+	  });
+  }
+
+  function darkenLogo(el) {
+      el.animate({fill: "#908e8e"}, 1000, brightenLogo);
+  }
+  
+  brightenLogo();
+ 
+}
+
+$(function(){
+    var handler = setInterval(function () {
+            $.ajax({
+                method: 'GET',
+                url: '/'
+            }).success(function (data) {
+                if (/Pixelated Mail/g.test(data)) {
+                    window.location.reload(true);
+                }
+            });
+        }, 5000);
+
+    $('#hive-section').height($(window).height())
+});

service/pixelated/bitmask_libraries/config.py
@@ -44,7 +44,7 @@ class LeapConfig(object):
 
     def __init__(self, leap_home=DEFAULT_LEAP_HOME, ca_cert_bundle=AUTO_DETECT_CA_BUNDLE, verify_ssl=True,
                  fetch_interval_in_s=30,
-                 timeout_in_s=15, start_background_jobs=True, gpg_binary=discover_gpg_binary(), certs_home=None):
+                 timeout_in_s=15, start_background_jobs=False, gpg_binary=discover_gpg_binary(), certs_home=None):
         """
         Constructor.
 

commit c49aa556e3ea0f152478f455ab2859d30acede7d

(Re-)added leap home parameter to soledad initialization.
- Problem introduced with commit 26b85d8e52
- Issues #237 #232 #196

service/pixelated/config/__init__.py
@@ -59,7 +59,7 @@ def initialize():
         loading_app = loading(args)
 
         def init_soledad():
-            return init_soledad_and_user_key(app)
+            return init_soledad_and_user_key(app, args.home)
 
         def stop_loading_app(leap_session):
             d = loading_app.stopListening()

service/pixelated/config/soledad.py
@@ -17,10 +17,11 @@
 from pixelated.bitmask_libraries.session import open as open_leap_session
 
 
-def init_soledad_and_user_key(app):
+def init_soledad_and_user_key(app, leap_home):
     leap_session = open_leap_session(app['LEAP_USERNAME'],
                                      app['LEAP_PASSWORD'],
-                                     app['LEAP_SERVER_NAME'])
+                                     app['LEAP_SERVER_NAME'],
+                                     leap_home)
     soledad = leap_session.soledad_session.soledad
     soledad.sync(defer_decryption=False)
     leap_session.nicknym.generate_openpgp_key()