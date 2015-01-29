1ce785f50178c185e11a27b3462be340b58fe8f4
Christoph Kluenter <ckluente@thoughtworks.com>
2015-01-29 12:48:36 +0100

    don't timeout if download of phantomjs is slow

--- provisioning/modules/phantomjs/manifests/init.pp
+++ provisioning/modules/phantomjs/manifests/init.pp
@@ -3,6 +3,7 @@ class phantomjs {
     command => '/usr/bin/wget -O /var/local/phantomjs-1.9.8.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2',
     creates => '/var/local/phantomjs-1.9.8.tar.bz2',
     notify  => Exec['unpack_phantomjs'],
+    timeout => '0',
   }
   exec{'unpack_phantomjs':
     command     => '/bin/tar xvfj phantomjs-1.9.8.tar.bz2',
