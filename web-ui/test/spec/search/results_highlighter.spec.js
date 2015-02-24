d9eaa6b9468dded7813e880291f5e4cc209ad0c6
Alabê Duarte <aduarte@thoughtworks.com>
2015-02-24 00:45:25 -0300

    #192 - uses 'self' everywhere instead of 'this'

--- web-ui/test/spec/search/results_highlighter.spec.js
+++ web-ui/test/spec/search/results_highlighter.spec.js
@@ -7,9 +7,9 @@ describeComponent('search/results_highlighter', function () {
   });
 
   it('highlights only words that matches with the keywords given', function () {
-    this.setupComponent('<div id="text">Any one seeing too many open bugs</div>');
+    self.setupComponent('<div id="text">Any one seeing too many open bugs</div>');
 
-    this.component.attr = {keywords: ["any"]};
+    self.component.attr = {keywords: ["any"]};
     self.component.highlightResults(event, {where: '#text'});
 
     var highlightedWords = self.component.$node.find('.search-highlight').length;
