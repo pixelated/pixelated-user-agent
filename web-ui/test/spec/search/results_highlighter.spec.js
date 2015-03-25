4cdfcbe713b804442de651da9df9b560ede0fc5e
Lisa Junger <ljunger@thoughtworks.com>
2015-03-25 11:07:46 +0100

    small refactoring to unnecessary self maintaining reference to this.
    Issue #192

--- web-ui/test/spec/search/results_highlighter.spec.js
+++ web-ui/test/spec/search/results_highlighter.spec.js
@@ -1,18 +1,13 @@
 describeComponent('search/results_highlighter', function () {
   'use strict';
-  var self;
-
-  beforeEach(function () {
-    self = this;
-  });
 
   it('highlights only words that matches with the keywords given', function () {
     this.setupComponent('<div id="text">Any one seeing too many open bugs</div>');
 
     this.component.attr = {keywords: ["any"]};
-    self.component.highlightResults(event, {where: '#text'});
+    this.component.highlightResults(event, {where: '#text'});
 
-    var highlightedWords = self.component.$node.find('.search-highlight').length;
+    var highlightedWords = this.component.$node.find('.search-highlight').length;
 
     expect(highlightedWords).toEqual(1);
   });
