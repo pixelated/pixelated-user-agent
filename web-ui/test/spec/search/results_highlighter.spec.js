e5714103bd5930a83d6169f8cde4e25f9e136668
Bruno Wagner <bwgpro@gmail.com>
2015-03-27 17:24:10 -0300

    Fixed jshint warnings

--- web-ui/test/spec/search/results_highlighter.spec.js
+++ web-ui/test/spec/search/results_highlighter.spec.js
@@ -4,7 +4,7 @@ describeComponent('search/results_highlighter', function () {
   it('highlights only words that matches with the keywords given', function () {
     this.setupComponent('<div id="text">Any one seeing too many open bugs</div>');
 
-    this.component.attr = {keywords: ["any"]};
+    this.component.attr = {keywords: ['any']};
     this.component.highlightResults(event, {where: '#text'});
 
     var highlightedWords = this.component.$node.find('.search-highlight').length;
@@ -15,7 +15,7 @@ describeComponent('search/results_highlighter', function () {
   it('resets highlights when a new search is performed', function() {
     this.setupComponent('<div id="text">Any one seeing too many open bugs</div>');
 
-    this.component.attr = {keywords: ["any"]};
+    this.component.attr = {keywords: ['any']};
     this.component.highlightResults(event, {where: '#text'});
     $(document).trigger(Pixelated.events.search.resetHighlight);
 
