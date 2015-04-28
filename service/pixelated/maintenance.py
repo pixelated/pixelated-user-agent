74a2462f89d21a3182c78511ce665c5ec337f200
Jefferson Stachelski <jstachel@thoughtworks.com>
2015-04-28 12:00:56 -0300

    #337 Made some refactors to make the code more readable

--- service/pixelated/maintenance.py
+++ service/pixelated/maintenance.py
@@ -50,6 +50,82 @@ import pixelated.support.ext_keymanager_fetch_key
 import pixelated.support.ext_requests_urllib3
 
 
+def initialize():
+    args = parse_args()
+    app = App()
+
+    init_logging(args)
+    init_leap_cert(args)
+
+    if args.dispatcher or args.dispatcher_stdin:
+        config_dispatcher(app, args)
+    else:
+        config_user_agent(app, args)
+
+    init_events_server()
+    execute_command = create_execute_command(args, app)
+
+    reactor.callWhenRunning(execute_command)
+    reactor.run()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='pixelated maintenance')
+    parser_add_default_arguments(parser)
+    subparsers = parser.add_subparsers(help='commands', dest='command')
+    subparsers.add_parser('reset', help='reset account command')
+    mails_parser = subparsers.add_parser('load-mails', help='load mails into account')
+    mails_parser.add_argument('file', nargs='+', help='file(s) with mail data')
+
+    subparsers.add_parser('dump-soledad', help='dump the soledad database')
+    subparsers.add_parser('sync', help='sync the soledad database')
+
+    return parser.parse_args()
+
+
+def create_execute_command(args, app):
+    def execute_command():
+
+        def init_soledad():
+            return init_soledad_and_user_key(app, args.home)
+
+        def get_soledad_handle(leap_session):
+            soledad = leap_session.soledad_session.soledad
+
+            return leap_session, soledad
+
+        def soledad_sync(args):
+            leap_session, soledad = args
+
+            soledad.sync()
+
+            return args
+
+        d = deferToThread(init_soledad)
+        d.addCallback(get_soledad_handle)
+        d.addCallback(soledad_sync)
+        add_command_callback(args, d)
+        d.addCallback(soledad_sync)
+        d.addCallback(shutdown)
+        d.addErrback(shutdown_on_error)
+
+    return execute_command
+
+
+def add_callback(args, defer):
+    if args.command == 'reset':
+        defer.addCallback(delete_all_mails)
+    elif args.command == 'load-mails':
+        defer.addCallback(load_mails, args.file)
+    elif args.command == 'dump-soledad':
+        defer.addCallback(dump_soledad)
+    elif args.command == 'sync':
+        # nothing to do here, sync is already part of the chain
+        pass
+    else:
+        print 'Unsupported command: %s' % args.command
+
+
 def delete_all_mails(args):
     leap_session, soledad = args
     generation, docs = soledad.get_all_docs()
@@ -94,76 +170,13 @@ def dump_soledad(args):
     return args
 
 
-def initialize():
-    parser = argparse.ArgumentParser(description='pixelated maintenance')
-    parser_add_default_arguments(parser)
-    subparsers = parser.add_subparsers(help='commands', dest='command')
-    subparsers.add_parser('reset', help='reset account command')
-    mails_parser = subparsers.add_parser('load-mails', help='load mails into account')
-    mails_parser.add_argument('file', nargs='+', help='file(s) with mail data')
-
-    subparsers.add_parser('dump-soledad', help='dump the soledad database')
-    subparsers.add_parser('sync', help='sync the soledad database')
-
-    args = parser.parse_args()
-    app = App()
-
-    init_logging(args)
-    init_leap_cert(args)
-
-    if args.dispatcher or args.dispatcher_stdin:
-        config_dispatcher(app, args)
-    else:
-        config_user_agent(app, args)
-
-    init_events_server()
-
-    def execute_command():
-
-        def init_soledad():
-            return init_soledad_and_user_key(app, args.home)
-
-        def get_soledad_handle(leap_session):
-            soledad = leap_session.soledad_session.soledad
-
-            return leap_session, soledad
-
-        def soledad_sync(args):
-            leap_session, soledad = args
-
-            soledad.sync()
-
-            return args
-
-        d = deferToThread(init_soledad)
-        d.addCallback(get_soledad_handle)
-        d.addCallback(soledad_sync)
-        if args.command == 'reset':
-            d.addCallback(delete_all_mails)
-        elif args.command == 'load-mails':
-            d.addCallback(load_mails, args.file)
-        elif args.command == 'dump-soledad':
-            d.addCallback(dump_soledad)
-        elif args.command == 'sync':
-            # nothing to do here, sync is already part of the chain
-            pass
-        else:
-            print 'Unsupported command: %s' % args.command
-        d.addCallback(soledad_sync)
-        d.addCallback(shutdown)
-        d.addErrback(shutdown_on_error)
-
-    reactor.callWhenRunning(execute_command)
-    reactor.run()
-
-
 def shutdown(args):
     reactor.stop()
 
 
 def shutdown_on_error(error):
     print error
-    reactor.stop()
+    shutdown(None)
 
 if __name__ == '__main__':
     initialize()
