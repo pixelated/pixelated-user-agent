bc130c82cd2167c76c2f46cef2971b168fcc694e
Victor Shyba <victor.shyba@gmail.com>
2015-06-09 16:28:13 -0300

    clean unused imports after certs and app cleanup

--- service/pixelated/application.py
+++ service/pixelated/application.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 from twisted.internet import reactor
 from twisted.internet.threads import deferToThread
 from twisted.internet import defer
--- service/pixelated/bitmask_libraries/certs.py
+++ service/pixelated/bitmask_libraries/certs.py
@@ -14,8 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
 import os
-import requests
-import json
 
 
 class LeapCertificate(object):
