27ad1503842dd92d4d3a8697f3aec8140446780a
Victor Shyba <victor.shyba@gmail.com>
2015-07-17 18:34:43 +0200

    fix test_contacts on integration

--- service/test/integration/test_contacts.py
+++ service/test/integration/test_contacts.py
@@ -14,52 +14,48 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
 from test.support.integration import SoledadTestBase, MailBuilder
+from twisted.internet import defer
 import os
 import json
 
 
 class ContactsTest(SoledadTestBase):
 
+    @defer.inlineCallbacks
     def test_TO_CC_and_BCC_fields_are_being_searched(self):
         input_mail = MailBuilder().with_tags(['important']).build_input_mail()
-        self.add_mail_to_inbox(input_mail)
+        yield self.add_mail_to_inbox(input_mail)
 
-        d = self.get_contacts(query='recipient')
+        contacts = yield self.get_contacts(query='recipient')
 
-        def _assert(contacts):
-            self.assertTrue('recipient@to.com' in contacts)
-            self.assertTrue('recipient@cc.com' in contacts)
-            self.assertTrue('recipient@bcc.com' in contacts)
-        d.addCallback(_assert)
-        return d
+        self.assertTrue('recipient@to.com' in contacts)
+        self.assertTrue('recipient@cc.com' in contacts)
+        self.assertTrue('recipient@bcc.com' in contacts)
 
+    @defer.inlineCallbacks
     def test_FROM_address_is_being_searched(self):
         input_mail = MailBuilder().with_tags(['important']).build_input_mail()
-        self.add_mail_to_inbox(input_mail)
+        yield self.add_mail_to_inbox(input_mail)
 
-        d = self.get_contacts(query='Sender')
+        contacts = yield self.get_contacts(query='Sender')
 
-        def _assert(contacts):
-            self.assertIn('Formatted Sender <sender@from.com>', contacts)
-        d.addCallback(_assert)
-        return d
+        self.assertIn('Formatted Sender <sender@from.com>', contacts)
 
+    @defer.inlineCallbacks
     def test_trash_and_drafts_mailboxes_are_being_ignored(self):
-        self.add_multiple_to_mailbox(1, mailbox='INBOX', to='recipient@inbox.com')
-        self.add_multiple_to_mailbox(1, mailbox='DRAFTS', to='recipient@drafts.com')
-        self.add_multiple_to_mailbox(1, mailbox='SENT', to='recipient@sent.com')
-        self.add_multiple_to_mailbox(1, mailbox='TRASH', to='recipient@trash.com')
+        yield self.add_multiple_to_mailbox(1, mailbox='INBOX', to='recipient@inbox.com')
+        yield self.add_multiple_to_mailbox(1, mailbox='DRAFTS', to='recipient@drafts.com')
+        yield self.add_multiple_to_mailbox(1, mailbox='SENT', to='recipient@sent.com')
+        yield self.add_multiple_to_mailbox(1, mailbox='TRASH', to='recipient@trash.com')
 
-        d = self.get_contacts(query='recipient')
+        contacts = yield self.get_contacts(query='recipient')
 
-        def _assert(contacts):
-            self.assertTrue('recipient@inbox.com' in contacts)
-            self.assertTrue('recipient@sent.com' in contacts)
-            self.assertFalse('recipient@drafts.com' in contacts)
-            self.assertFalse('recipient@trash.com' in contacts)
-        d.addCallback(_assert)
-        return d
+        self.assertTrue('recipient@inbox.com' in contacts)
+        self.assertTrue('recipient@sent.com' in contacts)
+        self.assertFalse('recipient@drafts.com' in contacts)
+        self.assertFalse('recipient@trash.com' in contacts)
 
+    @defer.inlineCallbacks
     def test_deduplication_on_same_mail_address_using_largest(self):
         input_mail = MailBuilder().with_tags(['important']).build_input_mail()
 
@@ -69,41 +65,36 @@ class ContactsTest(SoledadTestBase):
         formatted_input_mail.with_bcc('Recipient Carbon <recipient@bcc.com>')
         formatted_input_mail = formatted_input_mail.build_input_mail()
 
-        self.add_mail_to_inbox(input_mail)
-        self.add_mail_to_inbox(formatted_input_mail)
+        yield self.add_mail_to_inbox(input_mail)
+        yield self.add_mail_to_inbox(formatted_input_mail)
 
-        d = self.get_contacts(query='Recipient')
+        contacts = yield self.get_contacts(query='Recipient')
 
-        def _assert(contacts):
-            self.assertEquals(3, len(contacts))
-            self.assertTrue('Recipient Principal <recipient@to.com>' in contacts)
-            self.assertTrue('Recipient Copied <recipient@cc.com>' in contacts)
-            self.assertTrue('Recipient Carbon <recipient@bcc.com>' in contacts)
-        d.addCallback(_assert)
-        return d
+        self.assertEquals(3, len(contacts))
+        self.assertTrue('Recipient Principal <recipient@to.com>' in contacts)
+        self.assertTrue('Recipient Copied <recipient@cc.com>' in contacts)
+        self.assertTrue('Recipient Carbon <recipient@bcc.com>' in contacts)
 
+    @defer.inlineCallbacks
     def test_bounced_addresses_are_ignored(self):
         to_be_bounced = MailBuilder().with_to('this_mail_was_bounced@domain.com').build_input_mail()
-        self.add_mail_to_inbox(to_be_bounced)
+        yield self.add_mail_to_inbox(to_be_bounced)
 
         bounced_mail_template = MailBuilder().build_input_mail()
-        bounced_mail = self.mailboxes.inbox.add(bounced_mail_template)
+        bounced_mail = yield (yield self.mailboxes.inbox).add(bounced_mail_template)
         bounced_mail.hdoc.content = self._bounced_mail_hdoc_content()
         bounced_mail.save()
         self.search_engine.index_mail(bounced_mail)
 
         not_bounced_mail = MailBuilder(
         ).with_tags(['important']).with_to('this_mail_was_not@bounced.com').build_input_mail()
-        self.add_mail_to_inbox(not_bounced_mail)
+        yield self.add_mail_to_inbox(not_bounced_mail)
 
-        d = self.get_contacts(query='this')
+        contacts = yield self.get_contacts(query='this')
 
-        def _assert(contacts):
-            self.assertNotIn('this_mail_was_bounced@domain.com', contacts)
-            self.assertNotIn("MAILER-DAEMON@domain.org (Mail Delivery System)", contacts)
-            self.assertIn('this_mail_was_not@bounced.com', contacts)
-        d.addCallback(_assert)
-        return d
+        self.assertNotIn('this_mail_was_bounced@domain.com', contacts)
+        self.assertNotIn("MAILER-DAEMON@domain.org (Mail Delivery System)", contacts)
+        self.assertIn('this_mail_was_not@bounced.com', contacts)
 
     def _bounced_mail_hdoc_content(self):
         hdoc_file = os.path.join(os.path.dirname(__file__), '..', 'unit', 'fixtures', 'bounced_mail_hdoc.json')
