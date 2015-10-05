48c72644f41967d83e8b58de55ed6764d8bebe7b
Bruno Wagner <bwagner@riseup.net>
2015-10-05 17:26:18 -0300

    Fixed interstitial resizing

--- service/pixelated/assets/Interstitial.js
+++ service/pixelated/assets/Interstitial.js
@@ -4,12 +4,13 @@ if ($('#hive').length) {
   var left_pos = img_width * .5;
 
   var pixelated = hive.path("M12.4,20.3v31.8l28,15.8l28-15.8V20.3l-28-15.8L12.4,20.3z M39.2,56.4l-16.3-9V27.9l16.3,9.3L39.2,56.4z M57.7,47.4l-16.1,9l0-19.2l16.1-9.4V47.4z M57.7,25.2L40.4,35.5L22.9,25.2l17.5-9.4L57.7,25.2z").transform("translate(319, 50)").attr("fill", "#908e8e");
-  var all = hive.group().transform("matrix(2, 0, 0, 2, "+(left_pos - 950)+", -40)");
+  var all = hive.group().transform("matrix(2, 0, 0, 2, -100, -100)");
 
   var height = 50;
   var width = 58;
-  var rows = $(window).height() / height;
+  var rows = (($(window).height() / height) / 2) + 1;
   var cols = (($(window).width() / width) / 2) + 1;
+
 
   for (var j = 0; j < rows; j++) {
     for (var i = 0; i < cols; i++) {
