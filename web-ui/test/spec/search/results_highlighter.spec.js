0f84c6354a116fd53628b23a56c1528c5dd3e8ef
Giovane <giovaneliberato@gmail.com>
2015-11-03 17:59:07 -0200

    [#508] Making search also work with substrings - pairing w/ @pereiragislene

--- web-ui/test/spec/search/results_highlighter.spec.js
+++ web-ui/test/spec/search/results_highlighter.spec.js
@@ -1,7 +1,7 @@
 describeComponent('search/results_highlighter', function () {
   'use strict';
 
-  it('highlights only words that matches with the keywords given', function () {
+  it('highlights words or parts of words that match with the keywords given', function () {
     this.setupComponent('<div id="text">Any one seeing too many open bugs</div>');
 
     this.component.attr = {keywords: ['any']};
@@ -9,7 +9,7 @@ describeComponent('search/results_highlighter', function () {
 
     var highlightedWords = this.component.$node.find('.search-highlight').length;
 
-    expect(highlightedWords).toEqual(1);
+    expect(highlightedWords).toEqual(2);
   });
 
   it('resets highlights when a new search is performed', function() {
