596dde6fe889fb2c729612d0430a6d0d56d2db4d
varac <varacanero@zeromail.org>
2016-02-08 18:17:07 +0100

    install bzip2 before installing phantomjs
    - Resolves: #597

--- provisioning/modules/phantomjs/manifests/init.pp
+++ provisioning/modules/phantomjs/manifests/init.pp
@@ -1,4 +1,7 @@
+# install phantomjs for unit tests
 class phantomjs {
+  package{['tar','bzip2']:}
+
   exec{'download_phantomjs':
     command => '/usr/bin/wget -O /var/local/phantomjs-1.9.8.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2',
     creates => '/var/local/phantomjs-1.9.8.tar.bz2',
@@ -9,6 +12,7 @@ class phantomjs {
     command     => '/bin/tar xvfj phantomjs-1.9.8.tar.bz2',
     cwd         => '/var/local/',
     refreshonly => true,
+    require     => [ Package['tar'], Package['bzip2'] ],
     notify      => Exec['install_phantomjs'],
   }
   exec{'install_phantomjs':
