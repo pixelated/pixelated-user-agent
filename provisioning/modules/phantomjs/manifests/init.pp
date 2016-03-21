6855ac9aaccd8882cd53cdd3da90a0e4d8489554
Bruno Wagner <bwagner@riseup.net>
2016-03-21 18:09:23 -0300

    Bumped phantomjs version to match snapci version
    This also fix the functional tests not passing in vagrant,
    the old phantomjs had trouble with keepalive requests that
    selenium uses, that was fixed in this version so we shouldn't
    find badstatusline errors anymore

--- provisioning/modules/phantomjs/manifests/init.pp
+++ provisioning/modules/phantomjs/manifests/init.pp
@@ -3,20 +3,20 @@ class phantomjs {
   package{['tar','bzip2']:}
 
   exec{'download_phantomjs':
-    command => '/usr/bin/wget -O /var/local/phantomjs-1.9.8.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2',
-    creates => '/var/local/phantomjs-1.9.8.tar.bz2',
+    command => '/usr/bin/wget -O /var/local/phantomjs-2.1.1.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2',
+    creates => '/var/local/phantomjs-2.1.1.tar.bz2',
     notify  => Exec['unpack_phantomjs'],
     timeout => '0',
   }
   exec{'unpack_phantomjs':
-    command     => '/bin/tar xvfj phantomjs-1.9.8.tar.bz2',
+    command     => '/bin/tar xvfj phantomjs-2.1.1.tar.bz2',
     cwd         => '/var/local/',
     refreshonly => true,
     require     => [ Package['tar'], Package['bzip2'] ],
     notify      => Exec['install_phantomjs'],
   }
   exec{'install_phantomjs':
-    command     => '/usr/bin/install /var/local/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/bin/phantomjs',
+    command     => '/usr/bin/install /var/local/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs',
     refreshonly => true,
   }
 }
