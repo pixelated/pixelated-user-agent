22751f8a4f1172ee8acc43a64561e43bc436328d
Zara Gebru <zgebru@thoughtworks.com>
2016-04-07 11:31:28 +0200

    Issue #644: add time passed since last commit in nav bar

--- web-ui/config/add_git_version.sh
+++ web-ui/config/add_git_version.sh
@@ -3,8 +3,10 @@
 TEMPLATE_FILE="app/js/generated/hbs/templates.js"
 
 COMMITISH=$(git rev-parse --short HEAD)
+COMMITDATE=$(git show -s --format=%cr)
 
 perl -pi -e "s/UNKNOWN_VERSION/$COMMITISH/" $TEMPLATE_FILE
+perl -pi -e "s/COMMIT_DATE/$COMMITDATE/" $TEMPLATE_FILE
 
 if [ ! -f "$TEMPLATE_FILE" ] ; then
     echo "file $TEMPLATE_FILE not found" 1>&2
