373fbde99b66add1875426983238d5e4a9d46b08
Zara Gebru <zgebru@thoughtworks.com>
2016-04-28 20:37:39 +0200

    Issue #678 fixing typo on the right date format

--- web-ui/config/add_git_version.sh
+++ web-ui/config/add_git_version.sh
@@ -3,7 +3,7 @@
 TEMPLATE_FILE="app/js/generated/hbs/templates.js"
 
 COMMITISH=$(git rev-parse --short HEAD)
-COMMITDATE=$(git show -s --format=%ci)
+COMMITDATE=$(git show -s --format=%cd)
 
 perl -pi -e "s/UNKNOWN_VERSION/$COMMITISH/" $TEMPLATE_FILE
 perl -pi -e "s/COMMIT_DATE/$COMMITDATE/" $TEMPLATE_FILE
