62639dfdf3e26f0a2346c623f4126f91f5d8ee64
Thais Siqueira <thais.siqueira@gmail.com>
2016-07-27 16:45:15 -0300

    Adds scripts to run load tests.

---
+++ service/test/load/runloadtests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REQUESTS=$(($1 * 4))
+
+for i in `seq 9`; do 
+    LEAP_PROVIDER=dev.pixelated-project.org locust \
+        -f /vagrant/service/test/load/login_and_browse.py \
+        --no-web --num-request=$REQUESTS \
+        --clients=$1  --hatch-rate=1 --host=http://localhost:3333
+    cp ~/MetricsTime /vagrant/service/test/metrics/${1}users-${2}emails-${i}.txt
+    rm ~/MetricsTime
+done
