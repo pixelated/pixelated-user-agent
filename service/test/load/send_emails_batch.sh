5de831ff21d188765ee68568938ae9f6b95850bc
Thais Siqueira <thais.siqueira@gmail.com>
2016-08-01 08:54:00 -0300

    fix script print

--- service/test/load/send_emails_batch.sh
+++ service/test/load/send_emails_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for user in `seq $1`; do
-	echo "Sending $2 mails for user loadtest$1"
+	echo "Sending $2 mails for user loadtest$user"
 	for email in `seq $2`; do
-	    sendemail -f loadtest${user}@dev.pixelated-project.org -t loadtest${user}@dev.pixelated-project.org -u Hello$email -m Ha$email
+	    sendemail -f loadtest${user}@dev.pixelated-Project.org -t loadtest${user}@dev.pixelated-project.org -u Hello$email -m Ha$email
 	done
 done
