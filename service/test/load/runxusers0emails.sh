b9eb841eae0c1c34816d0d74a631bea91388f89b
Tulio Casagrande <tcasagra@thoughtworks.com>
2016-08-05 17:53:35 -0300

    Remove missing variable and change number of requests

--- service/test/load/runxusers0emails.sh
+++ service/test/load/runxusers0emails.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-REQUESTS=$(($1 * 20))
+REQUESTS=$(($1 * 100))
 
 LEAP_PROVIDER=dev.pixelated-project.org locust \
     -f /vagrant/service/test/load/login_and_browse.py \
     --no-web --num-request=$REQUESTS \
     --clients=$1  --hatch-rate=1 --host=http://localhost:3333
-cp ~/MetricsTime /vagrant/service/test/load/metrics/xusers0emails/${1}users-0emails-${i}.txt
-rm ~/MetricsTime
+mv ~/MetricsTime /vagrant/service/test/load/metrics/xusers0emails/${1}users-0emails.txt
