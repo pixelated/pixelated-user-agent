dcbe33cf53d446b4e2439af0ffd87645a0975d91
Ola Bini <ola@olabini.se>
2016-08-11 16:20:55 -0300

    Fix documentation of blocking http login to reflect how it should and should not be used

--- service/test/load/blocking_http_login.py
+++ service/test/load/blocking_http_login.py
@@ -1,18 +1,20 @@
 # Locustfile to run load tests for users with no emails, measuring how long the full login takes for pre-created users.
 # Example usage:
-#    locust -f /vagrant/service/test/load/blocking_http_login.py --no-web --num-request=1 --clients=1 --hatch-rate=1 --only-summary --host=http://localhost:3333
-# The number of requests and number of clients should be the same number for this test
+#    locust -f /vagrant/service/test/load/blocking_http_login.py --no-web --num-request=1 --clients=1 --hatch-rate=1 --loglevel=WARNING --host=http://localhost:3333
+# The number of requests and number of clients should be the same number for this test.
+# This command will not give any output. That is on purpose - the times reported by locust for this CAN NOT be trusted.
+# This script should only be used to actually run a swarm of requests in order to see timings on the server.
 
 # Preconditions and behavior:
 # - Ensure enough load test users are created. This should be one more than the number of requests/users specified
 #   The format for users and password is "loadtestX" and "password_X" where X is the number of the user. The number starts on 1
-#   An easy way to ensure users is to run 
+#   An easy way to ensure users is to run
 #   python /vagrant/service/test/load/users.py --invite-code=bla-bla --number-11 --leap-provider=dev.pixelated-project.org
 # - Before running the tests, you need to start the pixelated user agent and wait until it settles down. You should also
 #   restart the agent before running another test, in order to ensure users are logged out and the measurements are independent.
 # - It's recommended to truncate ~/MetricsTime before running a test, and moving it away somewhere after each test, in order
 #   to get more specific timing information. Once you have more than one file, you can use the service/test/load/metrics/tabalute.rb
-#   Ruby script to collate the information from the different metrics files, and find the areas of login that 
+#   Ruby script to collate the information from the different metrics files, and find the areas of login that
 #   increase in an unreasonable way when number of users are increased.
 
 import itertools
