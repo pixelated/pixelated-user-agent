fc810575370ddda32826c69a9a8a7662009af5ae
Thais Siqueira <thais.siqueira@gmail.com>
2016-08-11 11:55:30 -0300

    Fixed blocking http calls to do login on tasks
    This also make sure that users don't try to login more than one
    time and add the xrsf token that we need to post to login

--- service/test/load/blocking_http_login.py
+++ service/test/load/blocking_http_login.py
@@ -8,44 +8,37 @@ from pixelated.resources.login_resource import LoginResource
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from users import User
 
-
-def noop(*arg, **kwargs):
-    print "Stats reset prevented by monkey patch!"
-
-RequestStats.reset_all = noop
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-LEAP_PROVIDER = os.environ.get('LEAP_PROVIDER', 'unstable.pixelated-project.org')
-INVITE_CODE = ''
+LEAP_PROVIDER = os.environ.get('LEAP_PROVIDER', 'dev.pixelated-project.org')
+INVITE_CODE = 'bsuq-ahzc'
 
-
-user_number = itertools.count(600)
-
+user_number = itertools.count(1)
 
 class UserBehavior(TaskSet):
     def __init__(self, *args, **kwargs):
         super(UserBehavior, self).__init__(*args, **kwargs)
         self.cookies = {}
 
-    def on_start(self):
-        self.login()
-
-    def login(self):
-        index = user_number.next()
-        load_test_user = User(index, LEAP_PROVIDER)
-        print '#USER ' + load_test_user._username
-        username, password = load_test_user.get_or_create_user(INVITE_CODE)
-        login_payload = {"username": username, "password": password}
-        response = self.client.post("/%s" % LoginResource.BASE_URL,
-                                    login_payload, timeout=3600000.0,  verify=False, cookies=self.cookies)
-
     @task(1)
     def index(self):
-        pass
-        #self.client.get("/", verify=False)
-
+        number = user_number.next()
+        username = 'loadtestx_%d' % number
+        password = 'password_%d' % number
+        print "Logging in user: %s" % username
+        login_payload = {"username": username, "password": password}
+        self.cookies["XSRF-TOKEN"] = "blablabla"
+        self.client.post("/%s" % LoginResource.BASE_URL,
+                         login_payload,
+                         timeout=3600000.0,
+                         verify=False,
+                         headers={
+                            'X-Requested-With': 'XMLHttpRequest',
+                            'X-XSRF-TOKEN': "blablabla"
+                        },
+                        cookies=self.cookies)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 5000
+    min_wait = 1000
     max_wait = 5000
