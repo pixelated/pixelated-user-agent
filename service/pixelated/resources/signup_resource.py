e8f7cddd6334bfda1c95ebe3e6b826d05ec25c08
Roald de Vries <rdevries@thoughtworks.com>
2016-12-08 16:29:40 +0100

    [#801] add POST method to server-side signup resource

--- service/pixelated/resources/signup_resource.py
+++ service/pixelated/resources/signup_resource.py
@@ -13,9 +13,14 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
+import json
 import pkg_resources
-from twisted.logger import Logger
 
+from twisted.internet.defer import inlineCallbacks
+from twisted.logger import Logger
+from twisted.web.server import NOT_DONE_YET
+
+from pixelated.register import register
 from pixelated.resources import BaseResource
 
 log = Logger()
@@ -23,5 +28,21 @@ log = Logger()
 
 class SignupResource(BaseResource):
 
+    isLeaf = True
+
+    def __init__(self, services_factory, provider):
+        BaseResource.__init__(self, services_factory)
+        self._provider = provider
+
     def render_GET(self, request):
         return pkg_resources.resource_stream('templates', 'signup.html')
+
+    @inlineCallbacks
+    def _render_POST(self, request):
+        form_data = json.loads(request.content)
+        yield register(form_data['username'], form_data['password'], self._provider, form_data['invite'])
+        request.finish()
+
+    def render_POST(self, request):
+        self._render_POST(request)
+        return NOT_DONE_YET
