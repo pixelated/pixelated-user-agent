df7c34ec46cf4f11699a4a673d6bffe50a5b216e
Anike Arni <aarni@thoughtworks.com>
2016-12-27 15:33:22 -0200

    Fixes jshint by adding semicolon in index.js

--- web-ui/app/js/index.js
+++ web-ui/app/js/index.js
@@ -1,18 +1,18 @@
-import compose from 'flight/lib/compose'
-import registry from 'flight/lib/registry'
-import advice from 'flight/lib/advice'
-import withLogging from 'flight/lib/logger'
-import debug from 'flight/lib/debug'
-import events from 'page/events'
-import initializeDefault from 'page/default'
-import 'js/monkey_patching/all'
+import compose from 'flight/lib/compose';
+import registry from 'flight/lib/registry';
+import advice from 'flight/lib/advice';
+import withLogging from 'flight/lib/logger';
+import debug from 'flight/lib/debug';
+import events from 'page/events';
+import initializeDefault from 'page/default';
+import 'js/monkey_patching/all';
 
-window.Pixelated = window.Pixelated || {}
-window.Pixelated.events = events
+window.Pixelated = window.Pixelated || {};
+window.Pixelated.events = events;
 
-compose.mixin(registry, [advice.withAdvice, withLogging])
+compose.mixin(registry, [advice.withAdvice, withLogging]);
 
-debug.enable(true)
-debug.events.logAll()
+debug.enable(true);
+debug.events.logAll();
 
-initializeDefault('')
+initializeDefault('');
