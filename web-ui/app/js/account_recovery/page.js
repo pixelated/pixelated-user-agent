eab587344a275d7d4afb05b8682e4ff76df5da61
Tayane Fernandes <tayane.rmf@gmail.com>
2017-02-06 17:57:49 -0200

    [#922] Make backup page responsive for big devices
    with @anikarni

--- web-ui/app/js/account_recovery/page.js
+++ web-ui/app/js/account_recovery/page.js
@@ -4,11 +4,11 @@ import 'scss/account_recovery/page.scss'
 
 const Page = () => (
   <div className='container'>
+    <img src='assets/images/forgot-my-password.svg' alt="Esqueci minha senha!"/>
     <form>
-      <img src='assets/images/forgot-my-password.svg' alt="Esqueci minha senha!"/>
       <h1>E se você esquecer sua senha?</h1>
       <p>Informe outro e-mail que você usa regularmente. Esse será o seu e-mail de recuperação.</p>
-      <p>Instruções serão enviadas para esse e-mail, guarde com carinho.</p>
+      <p>Instruções para recuperar sua senha serão enviadas para esse e-mail, guarde com carinho.</p>
       <div className="field-group">
         <input type="text" name="email" className="email" required/>
         <label className="animated-label" htmlFor="email">Digite seu e-mail de recuperação</label>
