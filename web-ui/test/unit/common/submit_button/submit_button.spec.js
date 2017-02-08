16190197f72ea242ae239dc2741e2887c7008dfa
Anike Arni <aarni@thoughtworks.com>
2017-02-08 10:55:10 -0200

    [#922] Add test coverage for new js files
    Since this added a lot of scripts to our npm scripts, this commit also
    organizes the package.json. with @tayanefernandes

---
+++ web-ui/test/unit/common/submit_button/submit_button.spec.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme'
+import expect from 'expect'
+import React from 'react'
+import SubmitButton from 'src/common/submit_button/submit_button'
+
+describe('SubmitButton', () => {
+  let submitButton
+
+  beforeEach(() => {
+    submitButton = shallow(<SubmitButton buttonText="Add Email" />)
+  })
+
+  it('renders an input of type submit for add email', () => {
+    expect(submitButton.find('input[type="submit"]').props().value).toEqual('Add Email')
+  })
+})
